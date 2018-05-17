88835a289ebea1601e418e37ee30d378852adc2f
Kevin Marcellier <k.marcellier@gmail.com>
2018-05-17 11:30:48 +0200

    Init pom and main

---
+++ projets-java/account/src/main/java/fr/apside/entretien/account/Main.java
@@ -0,0 +1,8 @@
+package fr.apside.entretien.account;
+
+public class Main {
+
+    public static void main (String[] args){
+        System.out.println("Welcome to Apside");
+    }
+}
---
+++ projets-java/account/src/test/java/fr/apside/entretien/account/MainTest.java
@@ -0,0 +1,5 @@
+package fr.apside.entretien.account;
+
+class MainTest {
+
+}
